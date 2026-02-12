ba8c940cca7234e76ff9d4554278c57cd2daaac3
Thembelihle Nkosi <126172883+Thembelihle-tech@users.noreply.github.com>
2026-02-12 14:07:20 +0200

    Add files via upload

---
+++ WHERE Clause.sql
@@ -0,0 +1,74 @@
+-- WHERE Clause
+
+SELECT *
+FROM parks_and_recreation.employee_salary;
+
+SELECT *
+FROM parks_and_recreation.employee_demographics;
+
+SELECT *
+FROM employee_salary
+WHERE first_name = 'Leslie';
+
+SELECT *
+FROM employee_salary
+WHERE salary > 50000;
+
+SELECT *
+FROM employee_salary
+WHERE salary <= 50000;
+
+SELECT *
+FROM employee_demographics
+WHERE gender = 'Female';
+
+SELECT *
+FROM employee_demographics
+WHERE gender != 'Female';
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01';
+
+-- AND OR NOT -- Logical Operators
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+AND gender = 'male';
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+OR NOT gender = 'male';
+
+-- With parentheses
+SELECT *
+FROM employee_demographics
+WHERE (first_name = 'Leslie' AND age = 44) OR age > 55;
+
+-- LIKE statement 
+-- % and _
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a%';
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE '%er%';
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a__'; 
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a___%'; 
+
+
+
+
+
+
+
+
